78b4cfa92b03d003c66ba8dcf68a4ea8b93d931a
Victor G <vesbru@yahoo.fr>
2022-11-21 20:58:04 -0800

    finished SMS

--- main.py
+++ main.py
@@ -1,52 +1,77 @@
 from passgate import PassgateAPI
 from flask import Flask, request, abort
 import os
-from twilio.rest import Client
-from dotenv import load_dotenv
-from twilio.twiml.voice_response import Gather, VoiceResponse
-
 
 ASSETS_DIR = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 pAPI = PassgateAPI()
+
 
 def authorize():
     auth = request.headers['Authorization']
     if auth is None:
         return False
     else:
-        title,token = auth.split(" ")
+        title, token = auth.split(" ")
         if title is None or title != "Bearer" or token is None:
             return False
         return pAPI.authorizeClient(token), token
 
+
 @app.route("/requestcode", methods=['GET'])
 def getcode():
-    auth,client_api_token = authorize()
+    auth, client_api_token = authorize()
     if not auth:
         abort(403)
-    uname = request.args['username']
     phone = request.args['phone']
     timeout = request.args['to']
-    if uname is None or phone is None or timeout is None:
+    if phone is None or timeout is None:
         abort(400)
-    code = pAPI.setCode(client_api_token,uname,phone,int(timeout))
+    code = pAPI.setCode(client_api_token, phone, int(timeout))
     if code is None:
         abort(400)
     return code
 
-@app.route("/auth/<user_token>",methods=['GET'])
+
+@app.route('/requestsms', methods=['GET'])
+def requestsms():
+    auth, client_api_token = authorize()
+    if not auth:
+        abort(403)
+    phone = request.args['phone']
+    if phone is None:
+        abort(400)
+    token = pAPI.reqSMS(client_api_token, phone)
+    if token is None:
+        abort(400)
+    return token
+
+
+@app.route("/auth/<user_token>", methods=['GET'])
 def authenticate(user_token):
     auth, client_api_token = authorize()
     if not auth:
         abort(403)
     ret = pAPI.makeCall(user_token)
-    return {"authorized":ret}
+    return {"authorized": ret}
+
+
+@app.route("/auth/<user_token_SMS>/SMS", methods=['GET'])
+def verify_SMS_code(user_token_SMS):
+    auth, client_api_token = authorize()
+    if not auth:
+        abort(403)
+    code = str(request.args.get('code'))
+    ret = pAPI.verifySMS(user_token_SMS, code)
+    return {"authorized": ret}
+
 
 @app.route("/twilio_answer/<twiliotoken>", methods=['POST'])
 def twilio_answer(twiliotoken):
-    pAPI.registerTwilioAnswer(twiliotoken,request.values['Digits'])
+    pAPI.registerTwilioAnswer(twiliotoken, request.values['Digits'])
+    return ''
+
 
 if __name__ == '__main__':
     context = ('ssl_certs/local_api_cert.crt', 'ssl_certs/local_api_key.key')
-    app.run(ssl_context=context,threaded=True, port=5000)
+    app.run(threaded=True, port=5000, ssl_context=context)
--- passgate.py
+++ passgate.py
@@ -2,13 +2,15 @@ import datetime
 import os
 import secrets
 import threading
+import pytz
 
 from twilio.rest import Client
 from dotenv import load_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
 from twilio.twiml.voice_response import Gather, VoiceResponse
 
-ngrok_address = "https://fea5-128-189-150-96.ngrok.io/"
+#ngrok http https://localhost:5000
+ngrok_address = "https://ac33-128-189-144-134.ngrok.io"+'/'
 
 class TwilioWrapper:
     def __init__(self):
@@ -30,10 +32,16 @@ class TwilioWrapper:
         resulting_to = requestTimeout-20
         twiml = "<Response>" \
                     "<Gather action=\""+ngrokPrefix + "twilio_answer/" +twilioToken+"\" method=\"POST\" input=\"dtmf\" numDigits=\"2\" timeout=\""+str(resulting_to)+"\">" \
-                        "<Say>Login request for "+clientName+"'s services.\nPlease input the 2 digits that have appeared on your screen in the following "+ str(resulting_to-2) +" seconds. </Say>"\
+                        "<Say>Login request for "+clientName+"'s services.\nPlease input the 2 digits that have appeared on your screen in the next "+ str(resulting_to-2) +" seconds. </Say>"\
                     "</Gather>" \
                 "</Response>"
         return twiml
+
+    def send_SMS(self,body,outgoing_num):
+        return self.client.messages.create(body=body,to=outgoing_num,from_=self.number_from)
+
+    def generateSMSBody(self, generatedCode, clientName):
+        return "New Authentication request for " + clientName + ".\nYour 6-digit code is "+ str(generatedCode)+"."
 
 
 class PassgateAPI:
@@ -41,7 +49,9 @@ class PassgateAPI:
         self.twilio = TwilioWrapper()
         self.clientsMap = self.loadClientsFromDB()
         self.userTokensMap = {}   # userResponseToken -> (generatedCode,phone#,timeout,clientName)
-        self.twilioTokensMap = {} # twilioTokem -> (generatedCode,authFlag,threading.Event)
+        self.twilioTokensMap = {}  # twilioToken -> (generatedCode,authFlag,threading.Event)
+        self.SMSuserTokensMap = {}  # SMSuserResponseToken -> (generatedCode)
+        self.__NUM_SMS_GIGITS = 6
         self.MIN_TIMEOUT = 40
         self.MAX_TIMEOUT = 60
         self.scheduler = BackgroundScheduler()
@@ -59,8 +69,8 @@ class PassgateAPI:
     # Generates code for this Client's user `u`
     # `p` is the Client's user phone number
     # `t` is the Client's requested timeout
-    def setCode(self,clientToken,u,p,t):
-        assert clientToken is not None and u is not None and p is not None and t is not None
+    def setCode(self,clientToken,p,t):
+        assert clientToken is not None and p is not None and t is not None
         actualTimeout = min(max(t,self.MIN_TIMEOUT),self.MAX_TIMEOUT)
         userResponseToken = secrets.token_urlsafe(32)
         while userResponseToken in self.userTokensMap.keys():
@@ -70,11 +80,11 @@ class PassgateAPI:
         assert clientName is not None
         self.userTokensMap[userResponseToken] = (generatedCode,p,actualTimeout, clientName)
         # Schedule the removal of the token
-        deschedule_date = datetime.datetime.now() + datetime.timedelta(seconds=actualTimeout)
-        try:
-            self.scheduler.add_job(func=self.removeUserTokenFromMap, trigger='date', run_date=deschedule_date, args=[self, userResponseToken])
-        except Exception as e:
-            return
+        deschedule_date = datetime.datetime.now(tz=pytz.timezone("US/Pacific")) + datetime.timedelta(seconds=actualTimeout)
+        #try:
+        #    self.scheduler.add_job(func=self.removeUserTokenFromMap, trigger='date', run_date=deschedule_date, args=[self, userResponseToken])
+        #except Exception as e:
+        #    return
         # Return appropriate json
         return {'code': generatedCode, 'timeout': actualTimeout, 'response_at': "auth/"+userResponseToken}
 
@@ -95,7 +105,6 @@ class PassgateAPI:
         twilioToken = secrets.token_urlsafe(32)
         while twilioToken in self.twilioTokensMap.keys():
             twilioToken = secrets.token_urlsafe(32)
-        self.twilioTokensMap.update({twilioToken:(code, False, evt)})
         # get the twiml
         generatedTwiml = self.twilio.generateTwiml(ngrok_address,twilioToken,timeout,clientName)
         # Before making the call, we can remove the userToken from the map - this blocks the Client (Bank, or anyone
@@ -103,13 +112,14 @@ class PassgateAPI:
         # that token - they will have to generate a new one (although for the same user) if they want to retry the call
         self.removeUserTokenFromMap(userToken)
         # Finally make the call
-        self.twilio.make_call(generatedTwiml,phone)
+        call = self.twilio.make_call(generatedTwiml,phone)
+        self.twilioTokensMap.update({twilioToken:(code, False, call, evt)})
         # wait for the call to finish
         evt.wait()
         # The call has finished, we can proceed by deleting the Twilio token from the Map, and returning the result
         result = self.twilioTokensMap[twilioToken]
         assert result is not None
-        (code, authFlag, evt) = result
+        (code, authFlag, call, evt) = result
         del self.twilioTokensMap[twilioToken]
         return authFlag
 
@@ -117,11 +127,28 @@ class PassgateAPI:
         assert twilioToken is not None and digits is not None
         result = self.twilioTokensMap[twilioToken]
         assert result is not None
-        (code, authFlag, evt) = result
+        (code, authFlag, call, evt) = result
         auth = (int(code) == int(digits))
-        self.twilioTokensMap[twilioToken] = (code,auth,evt)
+        self.twilioTokensMap[twilioToken] = (code,auth, call,evt)
+        call.update(status='completed')
         evt.set() # This wakes up the `make_call` thread that was waiting.
         # TODO: we might want to pass around the timeout and wake it up as well in case Twilio doesn't respond
 
+    def reqSMS(self, clientToken, p):
+        assert clientToken is not None and p is not None
+        SMSuserResponseToken = secrets.token_urlsafe(32)
+        while SMSuserResponseToken in self.SMSuserTokensMap.keys():
+            SMSuserResponseToken = secrets.token_urlsafe(32)
+        generatedCode = ''.join([str(secrets.randbelow(10)) for x in range(self.__NUM_SMS_GIGITS)])
+        self.SMSuserTokensMap[SMSuserResponseToken] = generatedCode
+        clientName = self.clientsMap[clientToken]
+        assert clientName is not None
+        sms_object = self.twilio.send_SMS(self.twilio.generateSMSBody(generatedCode,clientName),p)
+        return {'token':SMSuserResponseToken}
 
-
+    def verifySMS(self,user_token_SMS,user_code):
+        assert user_token_SMS is not None and user_code is not None
+        actual_code = self.SMSuserTokensMap[user_token_SMS]
+        assert actual_code is not None
+        del self.SMSuserTokensMap[user_token_SMS]
+        return str(actual_code) == str(user_code)
